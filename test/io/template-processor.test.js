a90050323ad71e9a5a2d290d90be77e157025c1c
prunus-dev <prunus.dev@outlook.com>
2024-12-23 00:14:38 -0700

    Initiate template-processor

---
+++ test/io/template-processor.test.js
@@ -0,0 +1,14 @@
+import { expect } from "chai";
+import { describe, it } from "mocha";
+import { TemplateProcessor } from "../../lib/io/template-processor.js";
+
+describe("TemplateProcessor", function () {
+  describe("properties:", function () {});
+
+  describe("methods:", function () {
+    it("CASE HERE", function () {
+      new TemplateProcessor();
+      expect(1).to.equal(1);
+    });
+  });
+});
