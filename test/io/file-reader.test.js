cac8470bc186ff9ce50f8f3291ee3810992153a7
prunus-dev <prunus.dev@outlook.com>
2024-12-24 02:49:07 -0700

    File Reader initial
    > Created initial code and test for FileReader.
    > Add "glob" dependency in preparation of use to read file path globs.

---
+++ test/io/file-reader.test.js
@@ -0,0 +1,14 @@
+import { expect } from "chai";
+import { describe, it } from "mocha";
+import { FileReader } from "../../lib/io/file-reader.js";
+
+describe("FileReader", function () {
+  describe("properties:", function () {});
+
+  describe("methods:", function () {
+    it("CASE HERE", function () {
+      new FileReader();
+      expect(1).to.equal(1);
+    });
+  });
+});
